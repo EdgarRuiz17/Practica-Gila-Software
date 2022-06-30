b1f40e8f836f074dbe6b42fde65e36ac57c26ecb
Edgar Alfonso Arvayo Ruiz <edgar.ruiz.arvayo@gmail.com>
2022-06-29 19:50:51 -0700

    added mysql conecction to the database and routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,11 @@
-
 const express = require('express');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 const morgan = require('morgan');
 const app = express();
+
+//importing routes
+const customerRoutes = require('./routes/products');
 
 // settings
 app.set('port', process.env.PORT || 3000);
@@ -10,10 +14,16 @@ app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    database: 'gila_practice',
+    user: 'root',
+    password: ''
+}, 'single'));
 
 // routes
-app.use(require('./routes'));
-app.use('/api/products', require('./routes/products'));
+
+app.use('/api/products', customerRoutes);
 
 // starting the server
 app.listen(app.get('port'), () => {
