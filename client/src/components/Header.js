af8c82f7d60e2ea09ee3e2316c0b1404185018c0
Edgar Alfonso Arvayo Ruiz <edgar.ruiz.arvayo@gmail.com>
2022-06-30 20:01:54 -0700

    added header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header class="p-3 bg-dark text-white">
+            <div class="container">
+                <a href="/" class="align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    Gila Products
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
