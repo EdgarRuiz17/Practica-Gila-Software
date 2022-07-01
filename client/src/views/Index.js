4a70c35615c5914e9f035bd1435c6e21c54574e5
Edgar Alfonso Arvayo Ruiz <edgar.ruiz.arvayo@gmail.com>
2022-06-30 20:01:33 -0700

    added index module

---
+++ client/src/views/Index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//components
+import Header from '../components/Header'
+import Products from '../components/Products'
+
+const Index = () => {
+  return (
+    <div>
+        <Header/>
+        <section>
+            <Products/>
+        </section>
+    </div>
+  )
+}
+
+export default Index
