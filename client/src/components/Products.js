142594acf3d606726e940701b6bff6818a1026c9
Edgar Alfonso Arvayo Ruiz <edgar.ruiz.arvayo@gmail.com>
2022-07-02 21:54:01 -0700

    added button that directs to create products.

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 //components
 import Items from './Items'
 
 const Products = () => {
   return (
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div className="album py-5 bg-light">
+        <div className="container">
+            <div className='d-flex justify-content-center align-items-center'>
+                <Link className='btn btn-primary mb-4' to="/create">
+                    Add new product
+                </Link>
+            </div>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 <Items/>
             </div>
         </div>
