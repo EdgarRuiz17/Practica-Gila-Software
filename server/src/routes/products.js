2bc93741feda4025ba092136113ff5b34c9adefe
Edgar Alfonso Arvayo Ruiz <edgar.ruiz.arvayo@gmail.com>
2022-07-02 21:50:10 -0700

    created new routes for the query`s

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -6,6 +6,8 @@ const productController = require('../controllers/productController');
 
 router.get('/', productController.list);
 router.post('/add', productController.save);
+router.post('/add/attributes', productController.saveAttributes);
+router.get('/sku/:id', productController.listSKU);
 router.get('/update/:id', productController.edit);
 router.post('/update/:id', productController.update);
 router.get('/delete/:id', productController.delete);
